aacb6b0583803627025ecaef2e8ec39dabc8290b
DimaK9 <103372116+DimaK9@users.noreply.github.com>
2022-10-24 23:07:20 +0300

    7-10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
+// Напиши скрипт, який змінює кольори фону елемента <body> через 
+// інлайн-стиль по кліку на button.change-color і 
+// виводить значення кольору в span.color.
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const button = document.querySelector('button.change-color')
+const body = document.querySelector('body')
+const colorValue = document.querySelector('span.color')
+
+
+button.addEventListener('click', (event) => {
+  const changeColor = getRandomHexColor();
+  body.style.backgroundColor = changeColor;
+  colorValue.textContent = changeColor;
+
+  console.log(colorValue)
+})
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+const refs = {
+  containerRef: document.querySelector('#boxes'),
+  inputRef: document.querySelector('input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+};
+
+
+refs.createBtn.addEventListener('click', createBoxes);
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const newContainersArray = [];
+  amount = refs.inputRef.value;
+
+  for (let i = 0; i <= amount; i+=1) {
+    const newContainer = document.createElement('div');
+    newContainer.style.width = `${30+i*10}px`;
+    newContainer.style.height = `${30+i*10}px`;
+    newContainer.style.backgroundColor = getRandomHexColor();
+    
+    newContainersArray.push(newContainer);
+  }
+
+  refs.containerRef.append(...newContainersArray);
+}
+
+function destroyBoxes(){
+refs.containerRef.innerHTML = '';
 }
